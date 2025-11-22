130a4a848414ff5efd8988f06963b95652d4d7d0
sorasakuyu <884256028@qq.com>
2025-11-22 20:31:46 +0800

    修改链接

--- main.py
+++ main.py
@@ -47,5 +47,5 @@ def open_fullscreen(url):
 
 
 if __name__ == "__main__":
-    TARGET_URL = "https://dianm.teach.zhngjah.space/"
+    TARGET_URL = "https://dianm.edu.cynara.my/"
     open_fullscreen(TARGET_URL)
